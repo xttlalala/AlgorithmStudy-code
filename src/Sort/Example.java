c7e59219d798775ae6de6866d5c7a72d04206014
xttlalala <957382789@qq.com>
2022-09-03 10:30:14 +0800

    选择排序

---
+++ src/Sort/Example.java
@@ -0,0 +1,40 @@
+package Sort;
+
+import Tool.In;
+import Tool.StdIn;
+
+public class Example {
+    public static void sort(Comparable[] a){
+
+    }
+    private static boolean less(Comparable v,Comparable w){
+        return v.compareTo(w)<0;
+    }
+    private static void exch(Comparable[] a,int i,int j){
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+    private static void show(Comparable[] a){
+        for(Comparable c:a){
+            System.out.print(c+" ");
+        }
+        System.out.println();
+    }
+    private static boolean isSorted(Comparable[] a){
+        for(int i=1;i<a.length;i++){
+            if(less(a[i],a[i-1])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+
+    }
+}

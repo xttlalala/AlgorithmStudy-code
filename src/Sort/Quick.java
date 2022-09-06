6ee9226ef97ca121ee2083278daabfafad2269cf
xttlalala <957382789@qq.com>
2022-09-06 17:07:21 +0800

    快速排序

---
+++ src/Sort/Quick.java
@@ -0,0 +1,61 @@
+package Sort;
+
+import Tool.StdIn;
+import Tool.StdRandom;
+
+public class Quick {
+    public static void sort(Comparable[] a){
+        StdRandom.shuffle(a);
+        sort(a, 0, a.length-1);
+    }
+    public static void sort(Comparable[] a,int lo,int hi){
+        if(hi<=lo)
+            return;
+        int j = partition(a, lo, hi);
+        sort(a,lo,j-1);
+        sort(a,j+1,hi);
+    }
+    private static int partition(Comparable[] a,int lo,int hi){
+        int i = lo;
+        int j = hi+1;
+        Comparable v = a[lo];
+        while(true){
+            while(less(a[++i],v)) if(i==hi) break;
+            while(less(v,a[--j])) if(j==lo) break;
+            if(i>=j) break;
+            exch(a, i, j);
+        }
+        exch(a, lo, j);
+        return j;
+    }
+    private static boolean less(Comparable v,Comparable w){
+        return v.compareTo(w)<0;
+    }
+    private static void exch(Comparable[] a,int i,int j){
+        Comparable t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+    private static void show(Comparable[] a){
+        for(Comparable c:a){
+            System.out.print(c+" ");
+        }
+        System.out.println();
+    }
+    private static boolean isSorted(Comparable[] a){
+        for(int i=1;i<a.length;i++){
+            if(less(a[i],a[i-1])){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] a = StdIn.readAllStrings();
+        sort(a);
+        assert isSorted(a);
+        show(a);
+
+    }
+}
